65840d9aee2b75546b66fb0fbd5b4e679cd50e97
Andrew Bustos <andy.bustos1@yahoo.com>
2016-04-28 22:39:58 -0600

    Added a new Authentication service and implemented it in the registers.js script

--- frontend/app/scripts/controllers/register.js
+++ frontend/app/scripts/controllers/register.js
@@ -1,18 +1,12 @@
 'use strict';
 
-angular.module('webDev').controller('RegisterCtrl', function($scope, $rootScope, $http, alert, authToken, API_URL) {
+angular.module('webDev').controller('RegisterCtrl', function($scope,alert,auth) {
     $scope.submit = function() {
 
-        var url = API_URL + 'register' /*'http://localhost:3000/register'*/ ;
-        var user = {
-            email: $scope.email,
-            password: $scope.password
-        };
-
-        $http.post(url, user)
+        auth.register($scope.email, $scope.password)
             .success(function(res) {
                 alert('success', 'Account Created!', 'Welcome, ' + res.user.email + '!');
-                authToken.setToken(res.token);
+/*                authToken.setToken(res.token);*/
             })
             .error(function(err) {
                 alert('warning', 'Something went wrong :(', err.message);
--- frontend/app/scripts/services/auth.js
+++ frontend/app/scripts/services/auth.js
@@ -1,15 +1,24 @@
 'use strict';
 
 angular.module('webDev').service('auth', function($http, API_URL, authToken, $state) {
-    var url = API_URL + 'login';
+/*    var url = API_URL + 'login';*/
+
+    function authSuccessful(res) {
+        authToken.setToken(res.token);
+        $state.go('main');
+    }
 
     this.login = function(email, password) {
-        return $http.post(url, {
+        return $http.post(API_URL + 'login', {
             email: email,
             password: password
-        }).success(function(res) {
-            authToken.setToken(res.token);
-            $state.go('main');
-        })
+        }).success(authSuccessful);
+    }
+    
+    this.register = function(email, password) {
+        return $http.post(API_URL + 'register', {
+            email: email,
+            password: password
+        }).success(authSuccessful);
     }
 });
