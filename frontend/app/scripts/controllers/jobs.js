8e66eff25a70b67117e43834f163195efbe1b9e2
Andrew Bustos <andy.bustos1@yahoo.com>
2016-05-04 12:14:13 -0600

    added sails api to take care of authorization

--- frontend/app/scripts/controllers/jobs.js
+++ frontend/app/scripts/controllers/jobs.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('webDev').controller('JobsCtrl', function($scope, $http, API_URL, alert) {
-        $http.get(API_URL + 'jobs').success(function(jobs) {
+        $http.get('http://localhost:1337/jobs').success(function(jobs) {
             $scope.jobs = jobs;
         }).error(function(err) {
             alert('warning', 'Unable to get jobs', err.message);
---
+++ sapi/api/controllers/JobsController.js
@@ -0,0 +1,11 @@
+/**
+ * JobsController
+ *
+ * @description :: Server-side logic for managing jobs
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
